f8070712d1c94e9398a9025f666446afab3d139e
16bitsCreator <preussenglorien@gmail.com>
2024-06-29 13:34:15 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -165,12 +165,41 @@ addLayer("p", {
         }, 
         19: {
             title: "V8",
-            description: "Condensated points^1.2",
+            description: "Condensated points^1.2 + buyable",
             cost: new Decimal(1e20),
         unlocked(){ return hasUpgrade('c', 14)},
         }, 
-    } 
+    },
 
+buyables: {
+        11: {
+                title: "Nuclear Fusion",
+                unlocked() {
+                    return hasUpgrade('p', 19)
+                },
+                cost(x) {
+                    return new Decimal(1000).pow(x)
+                },
+                display() {
+                    let amount = getBuyableAmount('p', 11)
+                    return `
+                    <br /> You can't Access to that Milestone Right :/ Heres a little Boost.
+                    <br /><b><h3>Amount:</h3></b> ${formatWhole(amount)}
+                    <br /><b><h3>Currently boosting songs by:</h3></b> ${this.effectDisplay(temp.p.buyables[11].effect)}
+                    <br /><b><h3>Cost:</h3></b> ${format(temp.p.buyables[11].cost)} Notes`
+                },
+                canAfford() {
+                    return player.points.gte(this.cost())
+                },
+                buy() {
+                    player.points = player.points.sub(this.cost())
+                    setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
+                },
+                effect(x) {
+                    return Decimal.pow(2, x)
+                },
+                effectDisplay() {return format(buyableEffect(this.layer, this.id))},
+            },
 }),
 addLayer("c", {
     name: "Concentration", // This is optional, only used in a few places, If absent it just uses the layer id.
