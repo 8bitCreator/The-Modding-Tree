bc78b21db68dec63492579fe2305e3d9c846ab94
16bitsCreator <preussenglorien@gmail.com>
2024-06-30 09:37:34 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
+	name: "The Weird Tree",
 	id: "mymod",
-	author: "nobody",
+	author: "Freezy",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
