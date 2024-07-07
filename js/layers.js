07e79d9bd499e2b05e14087e966dc351014624c6
16bitsCreator <preussenglorien@gmail.com>
2024-07-07 16:44:42 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -42,7 +42,7 @@ description: "Food is Needed tho, Paleolithic Points Boost itself",
 cost: new Decimal(3),
 effect(){
 let eff = player.p.points.add(1).max(1)
-eff = decimal.log10(P13).add(10).max(10)
+eff = decimal.log10(eff).add(10).max(10)
 return eff
 },
 effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x"},
