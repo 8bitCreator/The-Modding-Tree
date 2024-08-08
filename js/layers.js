42c447072c5e2d38a268fd990389ce56be9d8272
16bitsCreator <preussenglorien@gmail.com>
2024-08-08 18:38:36 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -31,12 +31,9 @@ addLayer("p", {
 
         return mult; // Return the total multiplier
     },
-    
+
     gainExp() {
         let exp = new Decimal(1); // Start with a base exponent of 1
-
-        // You can check for additional upgrades affecting the exponent here if needed
-
         return exp; // Return the total exponent
     },
 
@@ -45,28 +42,45 @@ addLayer("p", {
     upgrades: {
         11: {
             title: "Stone Tools",
-            description: "Boost Primitive point by",
+            description: "Boost Primitive points by ",
             cost: new Decimal(5),
             effect() {
-                let eff = new Decimal(2); 
-                 if (hasUpgrade("p", 12)) eff = eff.mul(upgradeEffect("p", 12));
-                return eff  // Effect value for this upgrade 
-                     },
+                let eff = new Decimal(2);
+                if (hasUpgrade("p", 12)) eff = eff.mul(upgradeEffect("p", 12));
+                return eff; // Effect value for this upgrade 
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x"; },
+        },
 
-             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x"},
-                        },
-        
         12: {
             title: "Fire Discovery",
-            description: "Increase primitive point and last upgrade effect gain by ",
+            description: "Increase primitive points and last upgrade effect gain by ",
             cost: new Decimal(10),
             effect() {
-                 let eff = new Decimal(2); 
-                return eff  // Effect value for this upgrade
+                let eff = new Decimal(2);
+                return eff; // Effect value for this upgrade
             },
-          effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x"},
-            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x"; },
+        },
     },
-            
-        
+
+    buyables: {
+        11: {
+            title: "Power of the Ancients",
+            description: "Increase your primitive points by 5x each time you buy this. Cost is 10^x.",
+            cost(x) { return new Decimal(10).pow(x); }, // Cost is 10^x
+            effect() { return new Decimal(5); }, // Each buy increases points by 5x
+            display() {
+                return "Current Amount: " + getBuyableAmount(this.layer, this.id) + 
+                       "<br>Cost: " + format(this.cost(getBuyableAmount(this.layer, this.id).add(1))) + 
+                       " primitive points<br>" +
+                       "Effect: " + format(this.effect()) + "x primitive points";
+            },
+            canAfford() { return player[this.layer].points.gte(this.cost(getBuyableAmount(this.layer, this.id).add(1))); },
+            buy() {
+                player[this.layer].points = player[this.layer].points.sub(this.cost(getBuyableAmount(this.layer, this.id).add(1)));
+                addBuyables(this.layer, this.id, 1); // Increase the amount owned by 1
+            },
+        },
+    },
 });
