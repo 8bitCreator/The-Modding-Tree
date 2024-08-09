3c150d69e2a8f660c6701da8f97502093f36e75a
16bitsCreator <preussenglorien@gmail.com>
2024-08-09 16:49:21 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,26 +1,26 @@
 let modInfo = {
-	name: "The Technological Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "Knowledge",
-	modFiles: ["layers.js", "tree.js", "layers2.js"],
+    name: "The Technological Tree",
+    id: "mymod",
+    author: "nobody",
+    pointsName: "Knowledge",
+    modFiles: ["layers.js", "tree.js", "layers2.js"],
 
-	discordName: "",
-	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+    discordName: "",
+    discordLink: "",
+    initialStartPoints: new Decimal(10), // Used for hard resets and new players
+    offlineLimit: 1,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+    num: "0.0",
+    name: "Literally nothing",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+    <h3>v0.0</h3><br>
+        - Added things.<br>
+        - Added stuff.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
@@ -28,30 +28,35 @@ let winText = `Congratulations! You have reached the end and beaten this game, b
 // (The ones here are examples, all official functions are already taken care of)
 var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
 
-function getStartPoints(){
+function getStartPoints() {
     return new Decimal(modInfo.initialStartPoints)
 }
 
 // Determines if it should show points/sec
-function canGenPoints(){
-	return true
+function canGenPoints() {
+    return true
 }
 
 // Calculate points/sec!
 function getPointGen() {
-	if(!canGenPoints())
-		return new Decimal(0)
+    if (!canGenPoints())
+        return new Decimal(0)
 
-	let gain = new Decimal(1)
-	gain = gain.mul(buyableEffect("p", 11))
-	 // Multiply by the knowledge boost from the fire layer
-    
-	return gain
+    let gain = new Decimal(1)
+    gain = gain.mul(buyableEffect("p", 11)) // Multiply by the effect of buyable in the Primitive layer
+
+    // Multiply by the knowledge boost from the Fire layer if it's unlocked
+    if (player.f.unlocked) {
+        gain = gain.mul(layers.f.effect())
+    }
+
+    return gain
 }
 
-// You can add non-layer related variables that should to into "player" and be saved here, along with default values
-function addedPlayerData() { return {
-}}
+// You can add non-layer related variables that should go into "player" and be saved here, along with default values
+function addedPlayerData() { 
+    return {}
+}
 
 // Display extra things at the top of the page
 var displayThings = [
@@ -59,24 +64,20 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+    return player.points.gte(new Decimal("e280000000"))
 }
-
-
 
 // Less important things beyond this point!
 
 // Style for the background, can be a function
-var backgroundStyle = {
-
-}
+var backgroundStyle = {}
 
 // You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
+    return (3600) // Default is 1 hour which is just arbitrarily large
 }
 
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
 // you can cap their current resources with this.
-function fixOldSave(oldVersion){
+function fixOldSave(oldVersion) {
 }
