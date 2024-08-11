2f78aea9b9f8f291ff490235b32d8f27e59b418d
16bitsCreator <preussenglorien@gmail.com>
2024-08-11 15:14:23 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -37,10 +37,10 @@ addLayer("e", {
     update(diff) {
         // Calculate element gains per second
         let elementBoost = player.e.points.add(1).pow(0.5); // Boost based on Element Points
-        let fireGain = player.e.buyables[11].add(1).pow(0.1).mul(elementBoost).mul(diff);
-        let waterGain = player.e.buyables[12].add(1).pow(0.1).mul(elementBoost).mul(diff);
-        let earthGain = player.e.buyables[13].add(1).pow(0.1).mul(elementBoost).mul(diff);
-        let airGain = player.e.buyables[14].add(1).pow(0.1).mul(elementBoost).mul(diff);
+        let fireGain = player.e.buyables[11].add(1).pow(0.5).mul(elementBoost).mul(diff);
+        let waterGain = player.e.buyables[12].add(1).pow(0.5).mul(elementBoost).mul(diff);
+        let earthGain = player.e.buyables[13].add(1).pow(0.5).mul(elementBoost).mul(diff);
+        let airGain = player.e.buyables[14].add(1).pow(0.5).mul(elementBoost).mul(diff);
 
         // Add the calculated gains to the respective element totals
         player.e.fire = player.e.fire.add(fireGain);
@@ -51,10 +51,10 @@ addLayer("e", {
 
     effect() {
         return {
-            fireEffect: player.e.fire.add(1).pow(0.5), // Fire increases something
-            waterEffect: player.e.water.add(1).pow(0.5), // Water effect
-            earthEffect: player.e.earth.add(1).pow(0.5), // Earth effect
-            airEffect: player.e.air.add(1).pow(0.5), // Air effect
+            fireEffect: player.e.fire.add(1).pow(0.1), // Fire increases something
+            waterEffect: player.e.water.add(1).pow(0.1), // Water effect
+            earthEffect: player.e.earth.add(1).pow(0.1), // Earth effect
+            airEffect: player.e.air.add(1).pow(0.1), // Air effect
         }
     },
 
