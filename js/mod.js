488379d728caa12d5f7d952a641d7bd9807d70e4
16bitsCreator <preussenglorien@gmail.com>
2024-09-05 17:23:07 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The Universal Tree",
+	name: "The Replicanti Tree",
 	id: "mymod",
 	author: "nobody",
-	pointsName: "Energy Particles",
+	pointsName: "Time",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -13,71 +13,71 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Replicanti Expansion",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		- Added Replicanti growth mechanics.<br>
+		- Removed old tree mechanics.<br>
+		
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `Congratulations! You've reached the end of this version! More content coming soon.`
 
-// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
-// (The ones here are examples, all official functions are already taken care of)
+// Custom functions that shouldn't be called every tick
 var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
 
 function getStartPoints(){
     return new Decimal(modInfo.initialStartPoints)
 }
 
-// Determines if it should show points/sec
+// Determines if it should show points/sec (Energy Particles gain)
 function canGenPoints(){
 	return true
 }
 
-// Calculate points/sec!
+// Calculate Energy Particles gain per second (Replicanti boost applied here)
 function getPointGen() {
-	if(!canGenPoints())
+	if (!canGenPoints())
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	if (hasUpgrade("i", 12)) { 
-	gain = gain.mul(upgradeEffect("i", 12)); }
-	if (hasUpgrade("i", 13)) { 
-	gain = gain.mul(upgradeEffect("i", 13)); }
+
+	// Apply Replicanti effect to boost Energy Particle generation
+	if (player.r.points.gt(0)) {
+		gain = gain.times(tmp.r.effect); // tmp.r.effect is the Replicanti effect multiplier
+	}
+
 	return gain
 }
 
-// You can add non-layer related variables that should to into "player" and be saved here, along with default values
-function addedPlayerData() { return {
-}}
+// Non-layer related variables to be saved
+function addedPlayerData() { 
+	return {} 
+}
 
-// Display extra things at the top of the page
+// Extra things to display at the top of the page
 var displayThings = [
+	"Replicanti boosts Energy Particles gain."
 ]
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("e280000000")) // Example endgame threshold
 }
 
-
-
-// Less important things beyond this point!
-
-// Style for the background, can be a function
+// Style for the background (can be a function)
 var backgroundStyle = {
 
 }
 
-// You can change this if you have things that can be messed up by long tick lengths
+// Maximum tick length (to avoid long ticks breaking the game)
 function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
+	return(3600) // Default is 1 hour
 }
 
-// Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
-// you can cap their current resources with this.
+// Function to fix old saves
 function fixOldSave(oldVersion){
+	// Add logic here if needed for future save fixing
 }
