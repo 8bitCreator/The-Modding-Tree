38777595c895da106c80d763b9e66ceec80fa1ab
8bitCreator <167701429+8bitCreator@users.noreply.github.com>
2024-09-10 12:43:29 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,14 +1,14 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("m", {
+    name: "Mana", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#00FFFF",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    resource: "Mana points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -24,5 +24,27 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+upgrades: {
+        11: {
+            title: "Triple Your Points",
+            description: "Gain 3x more points.",
+            cost: new Decimal(100), // Cost of the upgrade (in mana points)
+            effect() {
+                let eff = new Decimal(3);
+		    return eff// 3x multiplier effect
+            },
+	    effectDisplay() { return "×" + format(upgradeEffect(this.layer, this.id)) },
+        },
+	12: {
+            title: "Add +1 Base in",
+            description: "Gain 3x more points.",
+            cost: new Decimal(100), // Cost of the upgrade (in mana points)
+            effect() {
+                let eff = new Decimal(3);
+		    return eff// 3x multiplier effect
+            },
+	    effectDisplay() { return "×" + format(upgradeEffect(this.layer, this.id)) },
+        },
+    },
 })
